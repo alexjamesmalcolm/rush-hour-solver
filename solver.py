45d914c369f2cce0f2499905565cd0108bb96ce4
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2025-04-07 11:55:15 -0400

    Added max turns and the constraint that a car can only be in one place per turn

--- solver.py
+++ solver.py
@@ -1,6 +1,9 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 from dataclasses import dataclass
-from rush_hour import RushHourGame, RushHourCar, Direction
+
+from pulp import LpProblem, LpVariable, LpBinary, lpSum, LpMinimize
+
+from rush_hour import RushHourGame, RushHourCar, Direction, Location
 
 
 @dataclass
@@ -9,5 +12,34 @@ class Movement:
     direction: Direction
 
 
-def solve_game(game: RushHourGame) -> List[Tuple[RushHourCar, Movement]]:
+def solve_game(
+    game: RushHourGame, max_turns: int = 100
+) -> List[Tuple[RushHourCar, Movement]]:
+    max_turns += 1
+    p = LpProblem("Solve_Rush_Hour_Game", sense=LpMinimize)
+
+    cars: Dict[RushHourCar, List[Dict[Location, LpVariable]]] = {
+        car: [
+            LpVariable.dicts(f"car_{car.id}_turn_{i}", game.grid, cat=LpBinary)
+            for i in range(max_turns)
+        ]
+        for car in game.cars
+    }
+
+    # Constraint: Each car can only be in one location per turn
+    for car in cars:
+        for turn in range(max_turns):
+            p += (
+                lpSum(cars[car][turn].values()) == 1,
+                f"car_{car.id}_on_turn_{turn}_must_be_in_one_location",
+            )
+
+    # Constraint: no horizontal car can exist on a row other than their original
+    # Constraint: no vertical car can exist on a column other than their original
+    # Constraint: no 2 cars can occupy the same cells on the same turn
+
+    # Assign scores for each turn
+    # p += lpSum(i *  for i in range(max_turns))
+
+    print(p)
     return [(game.cars[0], Movement(direction="Right", distance=3))]
