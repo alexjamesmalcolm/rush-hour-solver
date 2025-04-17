33829fa7d63ef5d1e167060663b4c2f786a55be9
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2025-04-17 16:09:25 -0400

    Added or and xor to lp_utils

--- lp_utils.py
+++ lp_utils.py
@@ -16,6 +16,35 @@ def lp_and(
     return constraints
 
 
+def lp_or(
+    a: LpVariable, b: LpVariable, answer: LpVariable, definition: Optional[str] = None
+) -> List[LpAffineExpression | Tuple[LpAffineExpression, str]]:
+    """Logical OR (not XOR) that I borrowed from: https://cs.stackexchange.com/a/12118"""
+    constraints: List[Tuple[LpAffineExpression, str]] = [
+        (answer <= a + b, f"{definition}__first_or"),
+        (answer >= a, f"{definition}__second_or"),
+        (answer >= b, f"{definition}__third_or"),
+    ]
+    if not definition:
+        return [constraint for constraint, _ in constraints]
+    return constraints
+
+
+def lp_xor(
+    a: LpVariable, b: LpVariable, answer: LpVariable, definition: Optional[str] = None
+) -> List[LpAffineExpression | Tuple[LpAffineExpression, str]]:
+    """Logical XOR (not OR) that I borrowed from: https://cs.stackexchange.com/a/12118"""
+    constraints: List[Tuple[LpAffineExpression, str]] = [
+        (answer <= a + b, f"{definition}__first_xor"),
+        (answer >= a - b, f"{definition}__second_xor"),
+        (answer >= b - a, f"{definition}__third_xor"),
+        (answer <= 2 - a - b, f"{definition}__fourth_xor"),
+    ]
+    if not definition:
+        return [constraint for constraint, _ in constraints]
+    return constraints
+
+
 def add_constraints(
     problem: LpProblem,
     constraints: List[LpAffineExpression | Tuple[LpAffineExpression, str]],
