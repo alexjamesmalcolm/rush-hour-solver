12b468893fa9c6fd2d09592b071e9dab3715dd5e
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2025-04-17 16:11:30 -0400

    Changed "definition" to "name"

--- lp_utils.py
+++ lp_utils.py
@@ -3,44 +3,44 @@ from pulp import LpProblem, LpVariable, LpAffineExpression
 
 
 def lp_and(
-    a: LpVariable, b: LpVariable, answer: LpVariable, definition: Optional[str] = None
+    a: LpVariable, b: LpVariable, answer: LpVariable, name: Optional[str] = None
 ) -> List[LpAffineExpression | Tuple[LpAffineExpression, str]]:
     """Logical AND that I borrowed from: https://cs.stackexchange.com/a/12118"""
     constraints: List[Tuple[LpAffineExpression, str]] = [
-        (answer >= a + b - 1, f"{definition}__first_and"),
-        (answer <= a, f"{definition}__second_and"),
-        (answer <= b, f"{definition}__third_and"),
+        (answer >= a + b - 1, f"{name}__first_and"),
+        (answer <= a, f"{name}__second_and"),
+        (answer <= b, f"{name}__third_and"),
     ]
-    if not definition:
+    if not name:
         return [constraint for constraint, _ in constraints]
     return constraints
 
 
 def lp_or(
-    a: LpVariable, b: LpVariable, answer: LpVariable, definition: Optional[str] = None
+    a: LpVariable, b: LpVariable, answer: LpVariable, name: Optional[str] = None
 ) -> List[LpAffineExpression | Tuple[LpAffineExpression, str]]:
     """Logical OR (not XOR) that I borrowed from: https://cs.stackexchange.com/a/12118"""
     constraints: List[Tuple[LpAffineExpression, str]] = [
-        (answer <= a + b, f"{definition}__first_or"),
-        (answer >= a, f"{definition}__second_or"),
-        (answer >= b, f"{definition}__third_or"),
+        (answer <= a + b, f"{name}__first_or"),
+        (answer >= a, f"{name}__second_or"),
+        (answer >= b, f"{name}__third_or"),
     ]
-    if not definition:
+    if not name:
         return [constraint for constraint, _ in constraints]
     return constraints
 
 
 def lp_xor(
-    a: LpVariable, b: LpVariable, answer: LpVariable, definition: Optional[str] = None
+    a: LpVariable, b: LpVariable, answer: LpVariable, name: Optional[str] = None
 ) -> List[LpAffineExpression | Tuple[LpAffineExpression, str]]:
     """Logical XOR (not OR) that I borrowed from: https://cs.stackexchange.com/a/12118"""
     constraints: List[Tuple[LpAffineExpression, str]] = [
-        (answer <= a + b, f"{definition}__first_xor"),
-        (answer >= a - b, f"{definition}__second_xor"),
-        (answer >= b - a, f"{definition}__third_xor"),
-        (answer <= 2 - a - b, f"{definition}__fourth_xor"),
+        (answer <= a + b, f"{name}__first_xor"),
+        (answer >= a - b, f"{name}__second_xor"),
+        (answer >= b - a, f"{name}__third_xor"),
+        (answer <= 2 - a - b, f"{name}__fourth_xor"),
     ]
-    if not definition:
+    if not name:
         return [constraint for constraint, _ in constraints]
     return constraints
 
