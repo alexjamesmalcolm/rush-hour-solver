1d77ebf61cf488dd8da44aafe6df569cfb3493a5
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2025-05-24 16:01:38 -0500

    Created test for Level One. I got this starting configuration from: https://rushhourgame.thinkfun.com

--- test/test_solver.py
+++ test/test_solver.py
@@ -43,3 +43,38 @@ class TestSolver(TestCase):
                 (player, Movement(direction="Right", distance=3)),
             ],
         )
+
+    def test_level_one(self):
+        """You can view the initial board state in docs/level-1.png"""
+        player = RushHourCar(id=0, col=0, row=2, width=2, height=1)
+        light_blue_car = RushHourCar(id=1, col=2, row=1, width=1, height=1)
+        light_purple_truck = RushHourCar(id=2, col=0, row=3, width=3, height=1)
+        blue_car = RushHourCar(id=3, col=0, row=4, width=1, height=2)
+        pink_car = RushHourCar(id=4, col=1, row=4, width=1, height=2)
+        purple_car = RushHourCar(id=5, col=2, row=4, width=1, height=2)
+        yellow_truck = RushHourCar(id=6, col=4, row=1, width=1, height=3)
+        orange_car = RushHourCar(id=7, col=5, row=2, width=1, height=2)
+        turquoise_car = RushHourCar(id=8, col=4, row=4, width=2, height=1)
+        grey_car = RushHourCar(id=9, col=4, row=5, width=2, height=1)
+
+        game = RushHourGame(
+            board_size=6,
+            cars=[
+                player,
+                light_blue_car,
+                light_purple_truck,
+                blue_car,
+                pink_car,
+                purple_car,
+                yellow_truck,
+                orange_car,
+                turquoise_car,
+                grey_car,
+            ],
+            goal=RushHourExit(col=5, row=2, exit_direction="Right"),
+        )
+
+        steps = solve_game(game, max_turns=50)
+        from pprint import pprint
+
+        pprint(steps)
